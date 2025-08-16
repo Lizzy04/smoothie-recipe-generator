85136031b25ebc4035eddcec807b31c05a1a0b1c
Lizzy04 <109428514+Lizzy04@users.noreply.github.com>
2025-08-16 16:31:26 +0200

    Added the API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Hello", "World",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "eb44271etfeaa70ae19b3f316ao64530";
+  let context =
+    "You are a professional nutrisionist who makes the best smoothies. Your task is to generate a short smoothie recipe in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
+  let prompt = `User instructions: Generate a smoothie recipe flavor ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeElement = document.querySelector("#recipe-generator-form");
