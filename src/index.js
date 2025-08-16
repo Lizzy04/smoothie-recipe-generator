adf116a37c1b3e5d20992e1c2b3132f88918c0eb
Lizzy04 <109428514+Lizzy04@users.noreply.github.com>
2025-08-16 16:01:08 +0200

    Added basic form handling and added Typewriter Plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Hello", "World",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeElement = document.querySelector("#recipe-generator-form");
+recipeElement.addEventListener("submit", generateRecipe);
